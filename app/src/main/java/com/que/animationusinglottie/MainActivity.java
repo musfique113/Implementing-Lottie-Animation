692979cdfbf45539dff12bce90d05249e683a3fc
musfique113 <musfique113@gmail.com>
2023-02-15 21:33:58 +0600

    basice lotti animation practice

--- app/src/main/java/com/que/animationusinglottie/MainActivity.java
+++ app/src/main/java/com/que/animationusinglottie/MainActivity.java
@@ -2,13 +2,24 @@ package com.que.animationusinglottie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    bt = findViewById(R.id.button);
+    bt.setOnClickListener(v ->{
+        Intent n = new Intent(getApplicationContext(), animationTwo.class);
+        startActivity(n);
+
+    });
     }
 }
