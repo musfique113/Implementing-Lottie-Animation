b76412cbdd86450497e8b3e4d5dc4aae7dbcb07f
musfique113 <musfique113@gmail.com>
2023-02-16 23:00:34 +0600

    basice lotti animation practice

---
+++ app/src/main/java/com/que/animationusinglottie/ANItHREE.java
@@ -0,0 +1,14 @@
+package com.que.animationusinglottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ANItHREE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anit_hree);
+    }
+}
--- app/src/main/java/com/que/animationusinglottie/animationTwo.java
+++ app/src/main/java/com/que/animationusinglottie/animationTwo.java
@@ -2,13 +2,22 @@ package com.que.animationusinglottie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class animationTwo extends AppCompatActivity {
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_two);
+        bt = findViewById(R.id.button);
+
+        bt.setOnClickListener(v ->{
+            Intent n = new Intent(getApplicationContext(), ANItHREE.class);
+            startActivity(n);
+        });
     }
 }
