19adf7a113a89ee6279d7ba8f5cb2ed774466661
musfique113 <musfique113@gmail.com>
2023-02-18 23:19:58 +0600

    on click animation

--- app/src/main/java/com/que/animationusinglottie/ANItHREE.java
+++ app/src/main/java/com/que/animationusinglottie/ANItHREE.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class ANItHREE extends AppCompatActivity {
+
+    boolean isCheckedDone = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anit_hree);
+
+        final LottieAnimationView lottieAnimationView = findViewById(R.id.checkDone);
+        lottieAnimationView.setOnClickListener(v ->{
+            if (isCheckedDone){
+                lottieAnimationView.setSpeed(1);
+                lottieAnimationView.playAnimation();
+                isCheckedDone = false;
+            } else {
+                lottieAnimationView.setSpeed(1);
+                lottieAnimationView.playAnimation();
+                isCheckedDone = true;
+            }
+        });
     }
 }
