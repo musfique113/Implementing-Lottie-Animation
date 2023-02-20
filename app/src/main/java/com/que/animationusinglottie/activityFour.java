31a13488fc5454fa78462103d8d9676bc7ad86b6
musfique113 <musfique113@gmail.com>
2023-02-20 21:10:31 +0600

    on click animation

---
+++ app/src/main/java/com/que/animationusinglottie/activityFour.java
@@ -0,0 +1,14 @@
+package com.que.animationusinglottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityFour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four);
+    }
+}
